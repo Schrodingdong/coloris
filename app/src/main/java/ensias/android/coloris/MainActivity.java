894eb86c9159fd563cbbf8fb7a1ccbbee2d0cbcb
Schrodingdong <91998001+Schrodingdong@users.noreply.github.com>
2023-04-02 14:50:53 +0000

    🚤 Added Fragment Navigation

--- app/src/main/java/ensias/android/coloris/MainActivity.java
+++ app/src/main/java/ensias/android/coloris/MainActivity.java
@@ -1,35 +1,29 @@
 package ensias.android.coloris;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
-import android.widget.Toast;
 
-import com.google.common.util.concurrent.ListenableFuture;
-
-import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import ensias.android.coloris.customservices.AppServices;
 import ensias.android.coloris.customservices.CameraService;
 import ensias.android.coloris.databinding.ActivityMainBinding;
+import ensias.android.coloris.ui.colorDetector.ColorDetectorFragment;
+import ensias.android.coloris.ui.hueShifter.HueShifterFragment;
+import ensias.android.coloris.ui.wiki.WikiFragment;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private ActivityMainBinding binding;
     private AppServices appServices;
+    private BottomNavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,15 +41,28 @@ public class MainActivity extends AppCompatActivity {
         appServices.addNewService(new CameraService(this, binding));
         appServices.startAllServices();
 
-        // TODO add navigation transition
-//        BottomNavigationView navView = binding.navView;
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_wiki, R.id.navigation_color_detector, R.id.navigation_hue_shifter)
-//                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(binding.navView, navController);
+        // Navigation menu
+        navView = binding.navView;
+        navView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.menu_color_detector:
+                    setCurrentFragment(new ColorDetectorFragment());
+                    break;
+                case R.id.menu_hue_shift:
+                    setCurrentFragment(new HueShifterFragment());
+                    break;
+                case R.id.menu_wiki:
+                    setCurrentFragment(new WikiFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void setCurrentFragment(Fragment f){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction
+                .replace(R.id.nav_host_fragment_activity_main, f)
+                .commit();
     }
 }
